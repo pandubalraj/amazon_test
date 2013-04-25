c39eec5b7d5fb5833de45ac1bf5b3cca1feb472f
pandurengaraj.b <pandurengaraj.b@sybrant.com>
2013-04-25 19:28:15 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserGuest::Application.config.secret_token = '565c62cd4fe674a5f38c98279baba0df015c626144a53b28fc4c699ace968e4d5e090c946cce0859fe630526c917268e772d0003f689d39b967c83047364251d'
